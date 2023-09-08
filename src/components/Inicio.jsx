2c7019997efa8e94bd899213b4a033ee3e40a863
felipelena8 <77079122+felipelena8@users.noreply.github.com>
2023-09-07 21:14:58 -0300

    Prueba

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Inicio() {
   return (
     <main className="flex flex-col justify-center items-center">
-      Prueba
+      Cambio
     </main>
   )
 }
