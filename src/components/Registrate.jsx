aa62180586bb96f49dc4956c1659d5f98570229b
felipelena8 <77079122+felipelena8@users.noreply.github.com>
2023-09-09 18:10:00 -0300

    Registro creado

--- src/components/Registrate.jsx
+++ src/components/Registrate.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export default function Registrate() {
   return (
-    <div>Registrate</div>
+    <div className='flex justify-center'>
+      <div className='flex mx-24 max-lg:mx-8 justify-around max-lg:flex-col gap-20 max-md:gap-3 items-center w-'>
+        <div className='font-bold text-6xl max-sm:text-3xl '>
+          ¡Da clases! <br/>
+          ¡Vivi de lo que te apasiona!
+        </div>
+        <div className='flex flex-col text-center p-8 justify-between gap-10 rounded-3xl shadow-lg max-lg:w-full'>
+          <span className='font-bold text-3xl'>Crea tu perfil</span>
+          <form action="" className='flex flex-col gap-2'>
+            <div className='flex gap-2'>
+              <input type="text" placeholder='Nombre' className='w-full bg-slate-300 placeholder-slate-500 rounded h-12 pl-2' />
+              <input type="text" placeholder='Apellido' className='w-full bg-slate-300 placeholder-slate-500 rounded h-12 pl-2' />
+            </div>
+            <input type="text" placeholder='Numero de telefono' className='bg-slate-300 placeholder-slate-500 rounded h-12 pl-2' />
+            <input type="text" placeholder='Correo electronico' className='bg-slate-300 placeholder-slate-500 rounded h-12 pl-2' />
+            <input type="text" placeholder='Contraseña' className='bg-slate-300 placeholder-slate-500 rounded h-12 pl-2' />
+            <button type="submit" className='mt-10 p-3 bg-rose-600 font-bold text-white text-lg rounded-xl'>Empezar ahora</button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
